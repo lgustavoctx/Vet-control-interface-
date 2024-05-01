61ec7cfbc13c4eab3b01f11956d2cfcd582163f0
Luís Filipe França <luisfranca@outlook.com>
2024-05-01 11:56:28 -0300

    teste api

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, request, jsonify
 import sqlite3
 import os
 
+app = Flask(__name__)
+
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-app = Flask(__name__, template_folder='templates', static_folder='static')
-
 DATABASE = os.path.join(BASE_DIR, 'data', 'database.db')
 
 def get_db_connection():
@@ -13,78 +12,68 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-@app.route('/')
-def index():
+@app.route('/tutors', methods=['GET'])
+def get_tutors():
     conn = get_db_connection()
-    tutores = conn.execute('SELECT * FROM tutors').fetchall()
-    animais = conn.execute('SELECT * FROM animais').fetchall()
-    veterinarios = conn.execute('SELECT * FROM veterinarios').fetchall()
+    tutors = conn.execute('SELECT * FROM tutors').fetchall()
     conn.close()
-    return render_template('/index.html', tutores=tutores, animais=animais, veterinarios=veterinarios)
+    tutor_list = [dict(tutor) for tutor in tutors]
+    return jsonify({'tutors': tutor_list})
 
-@app.route('/cadastrar-tutor')
-def cadastro_tutor():
+@app.route('/animals', methods=['GET'])
+def get_animals():
     conn = get_db_connection()
-    tutores = conn.execute('SELECT * FROM tutors').fetchall()
-    animais = conn.execute('SELECT * FROM animais').fetchall()
-    veterinarios = conn.execute('SELECT * FROM veterinarios').fetchall()
+    animals = conn.execute('SELECT * FROM animais').fetchall()
     conn.close()
-    return render_template('cadastrar-tutor/index.html', tutores=tutores, animais=animais, veterinarios=veterinarios)
+    animal_list = [dict(animal) for animal in animals]
+    return jsonify({'animals': animal_list})
+
+@app.route('/veterinarians', methods=['GET'])
+def get_veterinarians():
+    conn = get_db_connection()
+    veterinarians = conn.execute('SELECT * FROM veterinarios').fetchall()
+    conn.close()
+    veterinarian_list = [dict(veterinarian) for veterinarian in veterinarians]
+    return jsonify({'veterinarians': veterinarian_list})
 
 @app.route('/add_tutor', methods=['POST'])
 def add_tutor():
-    if request.method == 'POST':
-        nome = request.form['nome']
-        endereco = request.form['endereco']
-        modo_pagamento = request.form['modo_pagamento']
-        telefone = request.form['telefone']
-        email = request.form['email']
-        
+    data = request.json
+    if data:
         conn = get_db_connection()
         conn.execute('INSERT INTO tutors (nome, endereco, modo_pagamento, telefone, email) VALUES (?, ?, ?, ?, ?)',
-                     (nome, endereco, modo_pagamento, telefone, email))
+                     (data['nome'], data['endereco'], data['modo_pagamento'], data['telefone'], data['email']))
         conn.commit()
         conn.close()
-        return redirect(url_for('index'))
-    
-    return 'Method not allowed. Please use POST to submit a new tutor.'
+        return jsonify({'message': 'Tutor added successfully'}), 201
+    else:
+        return jsonify({'error': 'No data provided'}), 400
 
 @app.route('/add_animal', methods=['POST'])
 def add_animal():
-    if request.method == 'POST':
-        nome_tutor = request.form['Nome Tutor']
-        nome = request.form['nome']
-        peso = request.form['peso']
-        raca = request.form['raca']
-        tamanho = request.form['tamanho']
-        idade = request.form['idade']
-        problema_saude = request.form['problema_saude']
-        
+    data = request.json
+    if data:
         conn = get_db_connection()
         conn.execute('INSERT INTO animais (nome_tutor, nome, peso, raca, tamanho, idade, problema_saude) VALUES (?, ?, ?, ?, ?, ?, ?)',
-                     (nome_tutor, nome, peso, raca, tamanho, idade, problema_saude))
+                     (data['nome_tutor'], data['nome'], data['peso'], data['raca'], data['tamanho'], data['idade'], data['problema_saude']))
         conn.commit()
         conn.close()
-        return redirect(url_for('index'))
-    
-    return 'Method not allowed. Please use POST to submit a new animal.'
+        return jsonify({'message': 'Animal added successfully'}), 201
+    else:
+        return jsonify({'error': 'No data provided'}), 400
 
-@app.route('/add_veterinario', methods=['POST'])
-def add_veterinario():
-    if request.method == 'POST':
-        nome = request.form['nome']
-        especialidade = request.form['especialidade']
-        telefone = request.form['telefone']
-        email = request.form['email']
-        
+@app.route('/add_veterinarian', methods=['POST'])
+def add_veterinarian():
+    data = request.json
+    if data:
         conn = get_db_connection()
         conn.execute('INSERT INTO veterinarios (nome, especialidade, telefone, email) VALUES (?, ?, ?, ?)',
-                     (nome, especialidade, telefone, email))
+                     (data['nome'], data['especialidade'], data['telefone'], data['email']))
         conn.commit()
         conn.close()
-        return redirect(url_for('cadastro_tutor'))
-    
-    return 'Method not allowed. Please use POST to submit a new veterinarian.'
+        return jsonify({'message': 'Veterinarian added successfully'}), 201
+    else:
+        return jsonify({'error': 'No data provided'}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
